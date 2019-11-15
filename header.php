8479bd1fce0fbcb67867dcf8d2a05895b646d00b
Ji Min <jiminbestfriendboyfriend@gmail.com>
2019-11-15 23:54:12 +0700

    Update header.php

--- header.php
+++ header.php
@@ -8,10 +8,9 @@
 </div>
 <div class="menu">
 	<ul>
-		<li><a href="#"><i class="fa fa-shopping-cart"></i>Cart</a></li>
 		<li><a href="login.php">Log in</a></li>
 	</ul>	
 </div>
 </div>
 
-	<img src="images/1.gif" class="content">
+	<img src="https://www.chuu.co.kr/images/main/banners/top-area/main-banner/b/7.jpg" class="content">
