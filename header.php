9d379501278da8f570e07654ed9028db44b67fcf
Ji Min <jiminbestfriendboyfriend@gmail.com>
2019-11-16 00:02:16 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,8 +1,7 @@
 <div class="header">
-<div class="search">
 			<i class="fa fa-bars" id="menu-btn" onclick="openmenu()"></i>
 			<i class="fa fa-times" id="close-btn" onclick="closemenu()"></i>
-			<a href="homeasm.php"><img src="images/logo1.png" width="50" height="50" class="logo"></a>
+			<a href="index.php"><img src="images/logo1.png" width="50" height="50" class="logo"></a>
 			<input type="text" class="form-control">
 			<span class="input-group-text"><i class="fa fa-search"></i></span>
 </div>
